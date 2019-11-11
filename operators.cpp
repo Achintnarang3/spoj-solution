393aa3b7b03eb09392d0e5c3acb6c50cf336aebf
Achintnarang3 <41870104+Achintnarang3@users.noreply.github.com>
2019-11-12 00:59:49 +0530

    Create operators.cpp
    its a solution of operators (greedy) of spoj

---
+++ operators.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define return R
+#define vector<char> vc
+#define long long int lli
+
+using namespace std;
+void solve()
+{
+		lli n,s;
+	cin>>n>>s;
+	lli sum=  (n*(n+1))>>1;
+	
+	if(s>sum)
+	{
+		cout<<"Impossible";
+		R;
+	}
+	vc sign(n+1,'+');
+	if(sum%2!=s%2)
+	{
+		cout<<"Impossible";
+		R;
+	}
+	
+	for(int i=n;i>=2;i++)
+	{
+		
+		if(sum-2*i>=s&&sum-2*i!=s+2)
+		{
+			sum-=2*i;
+			sign[i]='-';
+		}
+	}
+	
+	if(sum!=s)
+	{
+			cout<<"Impossible";
+		R;
+		
+	}
+	cout<<"1";
+	for(int i=2;i<=n;i++)
+	cout<<sign[i]<<i;
+}
+
+int main() {
+	
+
+solve();
+	
+
+	return 0;
+}
